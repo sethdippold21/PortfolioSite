353eaede76e4b92e80f79d7cfd264d84e63c1784
sethdippold21 <sethdippold21@gmail.com>
2017-09-15 10:20:52 -0400

    Add popup instruction message

---
+++ assets/js/test.js
@@ -0,0 +1,10 @@
+
+(function($) {
+
+    $('.popup').fadeIn(400)//.delay(10000).fadeOut(400);  //fade in
+
+    $('.dismiss').click(function(){
+        $('.popup').fadeOut(200); 
+    })
+
+})(jQuery);
